20b1f7841fe4419c26f980623b9fcc80830aa947
Felipe dos Santos Bento <115020741+lobinhogameplays@users.noreply.github.com>
2023-03-27 21:06:27 -0300

    Add files via upload

---
+++ Atividade3/CalculadoraIMC/CalculadoraIMC/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace CalculadoraIMC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.labelAltura = new System.Windows.Forms.Label();
+            this.labelPeso = new System.Windows.Forms.Label();
+            this.txtIMC = new System.Windows.Forms.TextBox();
+            this.labelIMC = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(87, 237);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(146, 30);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(110, 65);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(100, 20);
+            this.txtAltura.TabIndex = 1;
+            this.txtAltura.TextChanged += new System.EventHandler(this.txtAltura_TextChanged);
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(110, 123);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(100, 20);
+            this.txtPeso.TabIndex = 2;
+            this.txtPeso.TextChanged += new System.EventHandler(this.txtPeso_TextChanged);
+            // 
+            // labelAltura
+            // 
+            this.labelAltura.AutoSize = true;
+            this.labelAltura.Location = new System.Drawing.Point(143, 43);
+            this.labelAltura.Name = "labelAltura";
+            this.labelAltura.Size = new System.Drawing.Size(34, 13);
+            this.labelAltura.TabIndex = 3;
+            this.labelAltura.Text = "Altura";
+            this.labelAltura.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelPeso
+            // 
+            this.labelPeso.AutoSize = true;
+            this.labelPeso.Location = new System.Drawing.Point(145, 103);
+            this.labelPeso.Name = "labelPeso";
+            this.labelPeso.Size = new System.Drawing.Size(31, 13);
+            this.labelPeso.TabIndex = 4;
+            this.labelPeso.Text = "Peso";
+            this.labelPeso.Click += new System.EventHandler(this.labelPeso_Click);
+            // 
+            // txtIMC
+            // 
+            this.txtIMC.Enabled = false;
+            this.txtIMC.Location = new System.Drawing.Point(110, 181);
+            this.txtIMC.Name = "txtIMC";
+            this.txtIMC.Size = new System.Drawing.Size(100, 20);
+            this.txtIMC.TabIndex = 5;
+            // 
+            // labelIMC
+            // 
+            this.labelIMC.AutoSize = true;
+            this.labelIMC.Location = new System.Drawing.Point(147, 162);
+            this.labelIMC.Name = "labelIMC";
+            this.labelIMC.Size = new System.Drawing.Size(26, 13);
+            this.labelIMC.TabIndex = 6;
+            this.labelIMC.Text = "IMC";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 317);
+            this.Controls.Add(this.labelIMC);
+            this.Controls.Add(this.txtIMC);
+            this.Controls.Add(this.labelPeso);
+            this.Controls.Add(this.labelAltura);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.Label labelAltura;
+        private System.Windows.Forms.Label labelPeso;
+        private System.Windows.Forms.TextBox txtIMC;
+        private System.Windows.Forms.Label labelIMC;
+    }
+}
+
---
+++ Atividade3/CalculadoraIMC/CalculadoraIMC/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraIMC
+{
+    public partial class Form1 : Form
+    {
+        double Altura, Peso;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!double.TryParse(txtAltura.Text, out Altura) || !double.TryParse(txtPeso.Text, out Peso))
+            {
+                MessageBox.Show("Valores Inválidos");
+            }
+            else
+            {
+                double IMC = Peso / (Altura * Altura);
+                txtIMC.Text = IMC.ToString("N2");
+                if (IMC < 18.5)
+                {
+                    MessageBox.Show("Classificação: Magreza");
+                }
+                if (IMC >= 18.5 && IMC <= 24.9)
+                {
+                    MessageBox.Show("Classificação: Normal");
+                }
+                if (IMC >= 25 && IMC <= 29.9)
+                {
+                    MessageBox.Show("Classificação: Sobrepeso");
+                    MessageBox.Show("Obesidade: Grau I");
+                }
+                if (IMC >= 30 && IMC <= 39.9)
+                {
+                    MessageBox.Show("Classificação: Obesidade");
+                    MessageBox.Show("Obesidade: Grau II");
+                }
+                if (IMC >= 40)
+                {
+                    MessageBox.Show("Classificação: Obesidade Grave");
+                    MessageBox.Show("Obesidade: Grau III");
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelPeso_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtAltura_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPeso_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
