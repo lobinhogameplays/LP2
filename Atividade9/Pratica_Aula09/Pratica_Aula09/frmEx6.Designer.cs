2e700beb1848ad7868a479d10c4d55ddefc7a097
Felipe dos Santos Bento <115020741+lobinhogameplays@users.noreply.github.com>
2023-04-25 21:44:01 -0300

    Add files via upload

---
+++ Atividade9/Pratica_Aula09/Pratica_Aula09/frmEx6.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace Pratica_Aula09
+{
+    partial class frmEx6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.lstbxNomes = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.Location = new System.Drawing.Point(134, 212);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(75, 23);
+            this.btnExecutar.TabIndex = 0;
+            this.btnExecutar.Text = "Executar";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // lstbxNomes
+            // 
+            this.lstbxNomes.FormattingEnabled = true;
+            this.lstbxNomes.Location = new System.Drawing.Point(340, 68);
+            this.lstbxNomes.Name = "lstbxNomes";
+            this.lstbxNomes.Size = new System.Drawing.Size(385, 316);
+            this.lstbxNomes.TabIndex = 1;
+            this.lstbxNomes.SelectedIndexChanged += new System.EventHandler(this.lstbxNomes_SelectedIndexChanged);
+            // 
+            // frmEx6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstbxNomes);
+            this.Controls.Add(this.btnExecutar);
+            this.Name = "frmEx6";
+            this.Text = "frmEx6";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.ListBox lstbxNomes;
+    }
+}
