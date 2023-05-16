f9c3c94628874388278f8d1898793ad45484df6d
Felipe dos Santos Bento <115020741+lobinhogameplays@users.noreply.github.com>
2023-05-16 11:01:28 -0300

    Add files via upload

---
+++ Prova/PNotas/PNotas/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace PNotas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.lbxRespostas = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(175, 207);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(75, 23);
+            this.btnVerificar.TabIndex = 0;
+            this.btnVerificar.Text = "button1";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.BtnVerificar_Click);
+            // 
+            // lbxRespostas
+            // 
+            this.lbxRespostas.CausesValidation = false;
+            this.lbxRespostas.FormattingEnabled = true;
+            this.lbxRespostas.Location = new System.Drawing.Point(315, 12);
+            this.lbxRespostas.Name = "lbxRespostas";
+            this.lbxRespostas.Size = new System.Drawing.Size(473, 433);
+            this.lbxRespostas.TabIndex = 1;
+            this.lbxRespostas.SelectedIndexChanged += new System.EventHandler(this.LbxRespostas_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbxRespostas);
+            this.Controls.Add(this.btnVerificar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.ListBox lbxRespostas;
+    }
+}
+
---
+++ Prova/PNotas/PNotas/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace PNotas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnVerificar_Click(object sender, EventArgs e)
+        {
+            char[,] alunos = new char[10, 10];
+            char[] gabarito = new char[10] { 'A', 'B', 'C', 'D', 'A', 'B', 'C', 'D', 'A', 'B'};
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                    if (!char.TryParse(Microsoft.VisualBasic.Interaction.InputBox($"Digite a {j + 1}º resposta para o {i + 1}º aluno:"), out alunos[i, j]))
+                    {
+                        MessageBox.Show("Entrada inválida");
+                        j--;
+                    }
+                    else if(alunos[i, j] != 'A' && alunos[i, j] != 'B' && alunos[i, j] != 'C' && alunos[i, j] != 'D' && alunos[i, j] != 'E')
+                    {
+                        MessageBox.Show("Entrada inválida");
+                        j--;
+                    }
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (alunos[i, j] == gabarito[j])
+                        lbxRespostas.Items.Add($"O aluno:{i+1} acertou a questão {j+1} era {gabarito[j]} escolheu {alunos[i, j]}");
+                    else
+                        lbxRespostas.Items.Add($"O aluno:{i+1} errou a questão {j+1} era {gabarito[j]} escolheu {alunos[i, j]}");
+                }
+
+            }
+        }
+
+        private void LbxRespostas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
